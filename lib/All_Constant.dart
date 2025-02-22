6c21fae0a1ee6164101ee9a26baddcad8de24268
i-saurabha-23 <saurabharalkar234@gmail.com>
2025-02-22 11:52:04 +0530

    Twilio Number Update ✅

--- lib/All_Constant.dart
+++ lib/All_Constant.dart
@@ -1,6 +1,6 @@
 const String Twilio_accountSid = 'AC6f83acf7e5877de4bdaab005ce8cdd57';
 const String Twilio_authToken = 'df221dd77ca6a07cf68ba4c7748f73a2';
-const String TwilioNumber = 'df221dd77ca6a07cf68ba4c7748f73a2';
+const String TwilioNumber = '+14583022915';
 
 
 const String GoogleAPiKey = 'AIzaSyCU8kcVOf8o1qnSXbSjMc104uTJfHGsnyU';
